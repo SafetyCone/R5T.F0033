c99ec5661ab84fe3f33534af082757b71ee98e67
davidcoats <david@getsafetycone.com>
2023-02-24 00:22:17 -0800

    Interim changes.

--- source/R5T.F0033/Code/Functionality/INotepadPlusPlusOperator.cs
+++ source/R5T.F0033/Code/Functionality/INotepadPlusPlusOperator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 using R5T.F0000;
 using R5T.T0132;
@@ -32,7 +33,28 @@ namespace R5T.F0033
 				enquotedFilePath);
         }
 
-		public void Open(IEnumerable<string> filePaths)
+        public void Open_WithDelay(
+            int delay_milliseconds,
+            IEnumerable<string> filePaths)
+        {
+            foreach (var filePath in filePaths)
+            {
+                this.Open(filePath);
+
+                Thread.Sleep(delay_milliseconds);
+            }
+        }
+
+        public void Open_WithDelay(
+            int delay_milliseconds,
+            params string[] filePaths)
+        {
+            this.Open_WithDelay(
+                delay_milliseconds,
+                filePaths.AsEnumerable());
+        }
+
+        public void Open(IEnumerable<string> filePaths)
 		{
             foreach (var filePath in filePaths)
             {
